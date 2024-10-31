39ed7b0eea90c0704f621f97aab5dbb2ce999aac
issaoak <85519921+issaoak@users.noreply.github.com>
2024-10-31 10:48:16 -0600

    Creamos la vista principal

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,6 @@
+@extends('plantilla')
+
+@section('content')
+<h1>Bienvenido a Biblioteca NewEra</h1>
+<p class="lead">Noticia Literaria: "La literatura y su impacto en la sociedad moderna"</p>
+@endsection
