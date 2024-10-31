629dddf95b4f23dee567a17601d05106e007dbab
issaoak <85519921+issaoak@users.noreply.github.com>
2024-10-31 16:48:04 -0600

    Hacemos el controlador para las validaciones

--- app/Http/Controllers/LibrosController.php
+++ app/Http/Controllers/LibrosController.php
@@ -15,6 +15,20 @@ class LibrosController extends Controller
     {
         return view('registro');
     }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'isbn' => 'required|numeric|digits:13',
+            'titulo' => 'required|string|max:150',
+            'autor' => 'required|string|max:100',
+            'paginas' => 'required|integer|min:1',
+            'año' => 'required|integer|min:1000|max:' . date('Y'),
+            'editorial' => 'required|string|max:100',
+            'email' => 'required|email'
+        ]);
+
+        return redirect()->route('registro')->with('success', 'Todo correcto: Libro "' . $request->titulo . '" guardado');
+    }
 
     
 }
